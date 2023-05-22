e94d3555ddca0ab79fc0410614434ef82857dcfe
Bigad Gaber <127905097+BigadG@users.noreply.github.com>
2023-05-22 16:46:41 -0500

    Finish second half of project

--- script.js
+++ script.js
@@ -1,78 +1,78 @@
-function getComputerChoice() {
-    let values = ["rock","paper","scissors"],
-    choice = values[Math.floor(Math.random() * values.length)];
-    return choice;
-  }
-  let computerSelection = getComputerChoice();
-  let selection = prompt("Rock, paper, or scissors?");
+  let values = ["rock","paper","scissors"]
   let x = 0
   let y = 0
   
-  
-  function playRound (selection, getComputerChoice) {
+  function playRound (selection, computerSelection) {
+    computerSelection = values[Math.floor(Math.random() * values.length)];
      let playerSelection = selection.toLowerCase();
   
-  
+     if (x >= 3) {
+        alert("You won the game!");
+        return location.reload();
+    }
+    else if (y >= 3) {
+        alert("You lost the game");
+        return location.reload();
+    }
+
      if (playerSelection == "rock") {
-         if (getComputerChoice == "rock") {
-             alert("It's a tie")  
+         if (computerSelection == "rock") {
+             return("It's a tie");
          }
-         else if (getComputerChoice == "paper") {
-             alert("You lost. Score: " + x + " to " + ++y)
+         else if (computerSelection == "paper") {
+             return("You lost. Score: " + x + " to " + ++y);
           }
-         else if (getComputerChoice == "scissors") {
-             alert("You won! Score: " + ++x + " to " + y)   
+         else if (computerSelection == "scissors") {
+             return("You won!! Score: " + ++x + " to " + y);
          }
      }
 
-
      else if (playerSelection == "paper") {
-        if (getComputerChoice == "rock") {
-            alert("You won! Score: " + ++x + " to " + y)
+        if (computerSelection == "rock") {
+            return("You won!! Score: " + ++x + " to " + y);
      
         }
-        else if (getComputerChoice == "paper") {
-            alert("It's a tie")
+        else if (computerSelection == "paper") {
+            return("It's a tie");
         }
-        else if (getComputerChoice == "scissors") {
-            alert("You lost. Score: " + x + " to " + ++y)
+        else if (computerSelection == "scissors") {
+            return("You lost. Score: " + x + " to " + ++y);
         }
     }
     else if (playerSelection == "scissors") {
-        if (getComputerChoice == "rock") {
-            alert("You lost. Score: " + x + " to " + ++y)
+        if (computerSelection == "rock") {
+            return("You lost. Score: " + x + " to " + ++y);
         }
-        else if (getComputerChoice == "paper") {
-            alert("You won! Score: " + ++x + " to " + y)
+        else if (computerSelection == "paper") {
+            return("You won!! Score: " + ++x + " to " + y);
    
         }
-        else if (getComputerChoice == "scissors") {
-            alert("It's a tie")
+        else if (computerSelection == "scissors") {
+            return("It's a tie");
         }
     }
     else {
-        alert("Invalid answer")
+        return("Invalid answer");
     }
-    return (x + " to " + y)
  }
  
+ computerSelection = values[Math.floor(Math.random() * values.length)];
+
+ const rockButton = document.querySelector('.Rock');
+ rockButton.addEventListener('click', () => {
+    const rockPlayRound = playRound('rock', computerSelection);
+    resultDisplay.textContent = (rockPlayRound);
+ });
  
- function gameResult() {
-    let playRoundVar = playRound(selection, getComputerChoice());
-    let playRoundX = Number(playRoundVar.charAt(0))
-    let playRoundY = Number(playRoundVar.charAt(5))
+ const paperButton = document.querySelector('.Paper');
+ paperButton.addEventListener('click', () => {
+    const paperPlayRound = playRound('paper', computerSelection);
+    resultDisplay.textContent = (paperPlayRound);
+ });
+
+ const scissorsButton = document.querySelector('.Scissors');
+ scissorsButton.addEventListener('click', () => {
+    const scissorsPlayRound = playRound('scissors', computerSelection);
+     resultDisplay.textContent = (scissorsPlayRound);
+ });
  
- 
-    if (playRoundX >= 2) {
-        alert("You won the game!")
-        location.reload()
-        return
-        }
-        else if (playRoundY >= 2) {
-            alert("You lost the game")
-           location.reload()
-            return
-            }
-     }
-     
-        
